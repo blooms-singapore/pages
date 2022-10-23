a9b55caae2c0258eac663c746094c58a2384bd24
wilicksnger <48726352+wilicksnger@users.noreply.github.com>
2022-10-23 23:55:01 +0800

    Update main.js

--- settings/main.js
+++ settings/main.js
@@ -2,4 +2,10 @@ const readMoreBtn = document.querySelector('.read-more-button');
 const text = document.querySelector(".text');
                                     
 readMoreBtn.addEventListener('click',(e)=>{
-  text.classList.toggle ('show-more')
+  text.classList.toggle ('show-more');
+  
+  if(readMoreBtn.innerText === 'Read More'){
+    readMoreBtn.innerText === 'Read Less'};
+    
+   else(readMoreBtn.innerText === 'Read Less'{
+   readMoreBtn.innerText === 'Read More'};
